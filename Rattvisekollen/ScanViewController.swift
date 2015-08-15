2e523725103847bf5bb8a466514f822a5ce0387f
Fredrik Bystam <fredrik.bystam@gmail.com>
2015-08-15 11:04:04 +0200

    setup some very basic starting files

---
+++ Rattvisekollen/ScanViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ScanViewController.swift
+//  Rattvisekollen
+//
+//  Created by Fredrik Bystam on 15/08/15.
+//  Copyright © 2015 nickenil_byrreb. All rights reserved.
+//
+
+import UIKit
+
+class ScanViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
